e98f971b7f06d72750bebb192352e65c784b80c9
aminghorbani <amin.ghorbani@live.com>
2015-01-05 08:25:24 +0330

    Update addliftController.js

--- src/main/webapp/app/controller/addliftController.js
+++ src/main/webapp/app/controller/addliftController.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 app.controller('addliftController', ['$scope', '$rootScope', '$location', 'Lift', 'liftService',
 function ($scope, $rootScope, $location, Lift, liftService) {
 
@@ -8,12 +8,26 @@ function ($scope, $rootScope, $location, Lift, liftService) {
     $scope.OneOffMonth = '01';
     $scope.OneOffDay = '01';
     
+    $scope.DepartureFlexibilityHour = '00';
+    $scope.DepartureFlexibilityMinute = '00';
+
+    $scope.ArrivalTimeHour = '00';
+    $scope.ArrivalTimeMinute = '00';
+
     $scope.addLift = function () {
         if (!$scope.addliftform.$invalid) {
+
+            $scope.lift.ShamsiDate = $scope.OneOffYear + $scope.OneOffMonth + $scope.OneOffDay;
+            $scope.lift.DepartureFlexibility = $scope.DepartureFlexibilityHour + $scope.DepartureFlexibilityMinute;
+            $scope.lift.ArrivalTime = $scope.ArrivalTimeHour + $scope.ArrivalTimeMinute;
+
             liftService.AddLift($scope.lift)
                 .then(function (result) {
                     alert('added');
                 });
+        }
+        else {
+            alert('Invalid');
         }
     };
 }]);
